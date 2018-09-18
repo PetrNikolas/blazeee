004bc055e1a2195af2ee6f419389a0f26fed4973
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-09-18 20:08:25 +0200

    update comments in accounts

--- lib/api/accounts/accounts.ex
+++ lib/api/accounts/accounts.ex
@@ -11,6 +11,9 @@ defmodule Api.Accounts do
   alias ApiWeb.Guardian
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
+  @doc """
+  Check if user is sign in and return current user
+  """
   def token_sign_in(email, password) do
     case email_password_auth(email, password) do
       {:ok, user} ->
@@ -20,11 +23,13 @@ defmodule Api.Accounts do
     end
   end
 
+  # JWT auth with email and password
   defp email_password_auth(email, password) when is_binary(email) and is_binary(password) do
     with {:ok, user} <- get_by_email(email),
     do: verify_password(password, user)
   end
 
+  # Get user by email
   defp get_by_email(email) when is_binary(email) do
     case Repo.get_by(User, email: email) do
       nil ->
@@ -35,6 +40,7 @@ defmodule Api.Accounts do
     end
   end
 
+  # Verify user password.
   defp verify_password(password, %User{} = user) when is_binary(password) do
     if checkpw(password, user.password_hash) do
       {:ok, user}
@@ -45,12 +51,6 @@ defmodule Api.Accounts do
 
   @doc """
   Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
   """
   def list_users do
     Repo.all(User)
@@ -58,31 +58,11 @@ defmodule Api.Accounts do
 
   @doc """
   Gets a single user.
-
-  Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_user!(123)
-      %User{}
-
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
-
   """
   def get_user!(id), do: Repo.get!(User, id)
 
   @doc """
   Creates a user.
-
-  ## Examples
-
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def create_user(attrs \\ %{}) do
     %User{}
@@ -92,15 +72,6 @@ defmodule Api.Accounts do
 
   @doc """
   Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def update_user(%User{} = user, attrs) do
     user
@@ -110,15 +81,6 @@ defmodule Api.Accounts do
 
   @doc """
   Deletes a User.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
   """
   def delete_user(%User{} = user) do
     Repo.delete(user)
@@ -126,12 +88,6 @@ defmodule Api.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{source: %User{}}
-
   """
   def change_user(%User{} = user) do
     User.changeset(user, %{})
