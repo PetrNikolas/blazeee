87a038bff480ca402ffec9ac0f6f56c6fbd2a12f
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-09-27 12:05:29 +0200

    update errors views

--- lib/api_web/views/error_view.ex
+++ lib/api_web/views/error_view.ex
@@ -1,16 +1,21 @@
 defmodule ApiWeb.ErrorView do
   use ApiWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Page not found"}}
+  end
+  
+  def render("422.json", _assigns) do
+    %{errors: %{detail: "Bad request"}}
+  end
+  
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal server error"}}
+  end
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  # In case no render clause matches or no
+  # template is found, let's render it as 500
+  def template_not_found(_template, assigns) do
+    render "500.json", assigns
   end
 end
